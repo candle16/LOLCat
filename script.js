e3ae5d8af6e350256a38febd4a1dfcf91e4ce46d
candle16 <kateskipton@gmail.com>
2018-04-28 14:32:33 -0400

    first functioning version

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var noon = 12;
 var evening = 18;
 
 var updateClock = function() {
-  
+
 var messageText;
 var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
 var message = document.getElementById('timeEvent');
@@ -80,97 +80,61 @@ var oneSecond = 1000;
 
 setInterval(updateClock, oneSecond);
 
-//set partytime button effects//
-//set up selection menu so that can ?redefine variables?//
+// set partytime button effects//
+
+var partyButton = document.getElementById('partyTimeButton');
+
+var ispartyTime = false;
+
+var partyEvent = function() {
+
+  if (ispartyTime == false) {
+
+    ispartyTime = true;
+
+    time = partyTime;
+
+  } else if (ispartyTime == true) {
+
+    ispartyTime = false;
+
+    time = new Date().getHours();
+  }
+  }
+
+partyButton.addEventListener('click', partyEvent);
+
+//selection menu for times//
+
+var wakeUpTimeSelector = document.getElementById('wakeUpTimeSelector');
+
+var wakeUpEvent = function() {
+
+  wakeupTime = wakeUpTimeSelector.value;
+}
+
+var lunchTimeSelector = document.getElementById('lunchTimeSelector');
+
+var lunchEvent = function() {
+
+  lunchTime = lunchTimeSelector.value;
+}
+
+var napTimeSelector = document.getElementById('napTimeSelector');
+
+var napEvent = function () {
+  napTime = napTimeSelector.value;
+}
+
+napTimeSelector.addEventListener('change', napEvent);
+
+wakeUpTimeSelector.addEventListener('change', wakeUpEvent);
+
+lunchTimeSelector.addEventListener('change', lunchEvent);
 
 
 
 
-
-
-
-
-
-
-
-
-// var noon = 12;
-// var evening = 18; // 6PM
-// var wakeupTime = 9; // 9AM
-// var lunchTime = 12; // 12PM
-// var partyTime = 17; // 5PM
-// var napTime = lunchTime + 2; // 2PM
-// var time = new Date().getHours();
-//
-//
-// var updateClock = function(){
-//
-// };
-//
-// var showCurrentTime = function() {
-//      // display the string on the webpage
-//      var clock = document.getElementById('clock');
-//
-//      var currentTime = new Date();
-//      var hours = currentTime.getHours();
-//      var minutes = currentTime.getMinutes();
-//      var seconds = currentTime.getSeconds();
-//      var meridian = "AM";
-//
-//      // Set Hours
-//      if (hours >= noon){
-//           meridian = "PM";
-//      }
-//
-//      if (hours > noon){
-//           hours = hours – 12;
-//      }
-//
-//      // Set Minutes
-//      if (minutes < 10){
-//           minutes = "0" + minutes;
-//      }
-//
-//      // Set Seconds
-//      if (seconds < 10){
-//           seconds = "0" + seconds;
-//      }
-//
-//      // put together the string that displays the time
-//      var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
-//
-//      clock.innerText = clockTime;
-// };
-//
-// showCurrentTime();
-// updateClock();
-//
-// var oneSecond = 1000;
-//
-// setInterval(updateClock, oneSecond);
-//
-// var partyTimeButton = document.getElementById("partyTimeButton");
-// var wakeUpTimeSelector = document.getElementById("wakeUpTimeSelector");
-// var lunchTimeSelector = document.getElementById("lunchTimeSelector");
-// var napTimeSelector = document.getElementById("napTimeSelector");
-//
-// var isPartyTime = false;
-//
-// var partyEvent = function() {
-//
-//    if (isPartyTime === false) {
-//       isPartyTime = true;
-//       time = partyTime;
-//       partyTimeButton.innerText = "PARTY TIME!";
-//       partyTimeButton.style.backgroundColor = "#222";
-//    } else {
-//       isPartyTime = false;
-//       time = new Date().getHours();
-//       partyTimeButton.innerText = "PARTY OVER";
-//       partyTimeButton.style.backgroundColor = "#0A8DAB";
-//    }
-// };
-//
 // var wakeUpEvent = function() {
 // 	wakeupTime = wakeUpTimeSelector.value;
 // };
